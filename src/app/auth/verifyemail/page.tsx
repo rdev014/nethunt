7b0bd46aa47208a08fed8bea0a7b31ab2bb683d1
rdev014 <rdev01431@gmail.com>
2024-11-30 15:50:27 +0530

    resolve error

--- src/app/auth/verifyemail/page.tsx
+++ src/app/auth/verifyemail/page.tsx
@@ -37,7 +37,7 @@ export default function VerifyEmail() {
     if (token) {
       verifyUserEmail();
     }
-  }, [token]);
+  }, [token,verifyUserEmail]);
 
   return (
     <div className="min-h-screen bg-gray-100 dark:bg-gray-900 text-gray-900 dark:text-gray-100 flex items-center justify-center ">
