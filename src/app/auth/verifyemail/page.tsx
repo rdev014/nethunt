615304838afb1734380eff5b37ea93165af8e44b
rdev014 <rdev01431@gmail.com>
2024-11-30 13:39:41 +0530

    modified code

--- src/app/auth/verifyemail/page.tsx
+++ src/app/auth/verifyemail/page.tsx
@@ -37,7 +37,7 @@ export default function VerifyEmail() {
     if (token) {
       verifyUserEmail();
     }
-  }, [token,verifyUserEmail]);
+  }, [token]);
 
   return (
     <div className="min-h-screen bg-gray-100 dark:bg-gray-900 text-gray-900 dark:text-gray-100 flex items-center justify-center ">
