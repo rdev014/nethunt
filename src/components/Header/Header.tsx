d6a2eaa067da1e749563bef18251b31d1980e260
rdev014 <rdev01431@gmail.com>
2024-12-02 12:41:03 +0530

    issue resolved

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -68,13 +68,15 @@ export default function Header() {
   }
 
   return (
-    <Disclosure as="nav" className="bg-gray-900 dark:bg-gray-800 shadow-lg  ">
+   <>
+   
+   <Disclosure as="nav" className="bg-gray-900 dark:bg-gray-800 shadow-lg  ">
       <div className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
         <div className="flex h-16 items-center justify-between">
           {/* Brand Name */}
           <div className="flex items-center">
             <div className="flex-shrink-0">
-             <Link href='/' className='text-orange-600 text-2xl font-extrabold'>NetHunt</Link>
+              <Link href='/' className='text-orange-600 text-2xl font-extrabold'>NetHunt</Link>
             </div>
           </div>
 
@@ -189,8 +191,11 @@ export default function Header() {
             </DisclosureButton>
           ))}
         </div>
-       
+
       </DisclosurePanel>
     </Disclosure>
+   
+   
+   </>
   )
 }
